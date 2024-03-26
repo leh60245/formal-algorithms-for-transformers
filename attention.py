0edf8bd44a83c6c7d4687dba1110a3337ae60b6e
sh3866 <littlepoo2000@gmail.com>
2024-03-26 18:44:08 +0900

    Basic Single Query Attention

---
+++ attention.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+class SingleQueryAttention(nn.Module):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        
+        
+    def forward(self, tokens):
+        pass
+    
